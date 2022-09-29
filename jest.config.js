c4eeb60efcea1f56a98f80c3f3591472d4cdb1fd
Martin Raifer <martin@raifer.tech>
2022-09-29 14:09:01 +0200

    update dependencies

--- jest.config.js
+++ jest.config.js
@@ -5,7 +5,7 @@ export default {
   verbose: true,
   transform: {},
   moduleNameMapper: {
-    "#ansi-styles": "chalk/source/vendor/ansi-styles/index.js",
-    "#supports-color": "chalk/source/vendor/supports-color/index.js"
+    "#ansi-styles": "<rootDir>/node_modules/chalk/source/vendor/ansi-styles/index.js",
+    "#supports-color": "<rootDir>/node_modules/chalk/source/vendor/supports-color/index.js"
   },
 };
